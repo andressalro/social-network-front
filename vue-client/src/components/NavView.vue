7d5582c4c2d476748a8a994fc32d385fccbebde8
Andrés Salgado Rojas <asalgado@darumasoftware.com>
2022-02-28 10:36:24 -0500

    Feature: Implementar cierre de sesión

--- vue-client/src/components/NavView.vue
+++ vue-client/src/components/NavView.vue
@@ -2,10 +2,10 @@
 
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Red social asalgado</a>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+    
     <ul class="navbar-nav px-3">
       <li class="nav-item text-nowrap">
-        <a class="nav-link" href="#">Cerrar sesion</a>
+        <a class="nav-link" href="javascript:void(0)" @click="logout">Cerrar sesion</a>
       </li>
     </ul>
   </nav>
@@ -13,9 +13,22 @@
 </template>
 
 <script>
+import {useRouter} from "vue-router";
 
 export default {
-    name: "NavView"
+    name: "NavView",
+    setup() {
+      const router = useRouter();
+
+      const logout = async () => {
+        localStorage.removeItem("JWT");
+        await router.push("/login");
+      }
+
+      return {
+        logout
+      }
+    }
 }
 
 </script>
